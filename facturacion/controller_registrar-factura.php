9053963e896fe72b151b6fd034cbeb0cebaac950
antonyhipolo <antonyhipolomachelo@gmail.com>
2025-05-26 21:18:22 -0500

    se agrego el modelo de facturas

--- facturacion/controller_registrar-factura.php
+++ facturacion/controller_registrar-factura.php
@@ -1,5 +1,6 @@
 <?php
 include('../app/config.php');
+include('literal.php');
 
 $id_informacion = $_GET['id_informacion'];
 $nro_factura = $_GET['nro_factura'];
@@ -9,12 +10,27 @@ $id_cliente = $_GET['id_cliente'];
 date_default_timezone_set("America/Lima");
 $fechaemision_fac = date("y-m-d");
 $Fecha_emision = $fechaemision_fac;
+
+date_default_timezone_set("America/Lima");
+$fechaHora = date("y-m-d h:i:s");
+
 //fecha de ingreso y salida
 $Fecha_ingreso = $_GET['Fecha_ingreso'];
 $Hora_ingreso = $_GET['Hora_ingreso'];
 $Fecha_salida = date('d/m/y');
 $Hora_salida = date('H:i');
-// calcula el tiempo del parqueo
+
+///////////////////calculo por días////////////////////////////////////////
+$Fecha_salida_ára_calcular = date('Y/m/d');
+$dato1 = new DateTime($Fecha_ingreso);
+$dato2 = new DateTime($Fecha_salida_ára_calcular);
+$días_calculado = $dato1->diff($dato2);
+$días_calculado->days;
+$día_imprimir_dif = $días_calculado->days;
+
+///////////////////////////////////////////////////////////////////////////
+
+//////////////////calcula el tiempo del parqueo////////////////////////////
 $c_hora_ingreso = strtotime($Hora_ingreso); 
 $c_hora_salida = strtotime($Hora_salida);
 $diferencia_de_horas = ($c_hora_salida - $c_hora_ingreso)/3600;
@@ -22,23 +38,75 @@ $hora_calculado = (int)$diferencia_de_horas;
 $diferencia_minutos = ($c_hora_salida - $c_hora_ingreso)/60;
 $calculando =$hora_calculado * 60;
 $minutos_calculado = $diferencia_minutos-$calculando;
-$tiempo = $hora_calculado." horas con ".$minutos_calculado." minutos";
+if ($día_imprimir_dif =="0") {
+     $tiempo =$hora_calculado." horas  y  ".$minutos_calculado." minutos";
+} else {
+    $tiempo =$día_imprimir_dif." días con ". $hora_calculado." horas  y  ".$minutos_calculado." minutos";
+}
+
 ////////////////////////////////////////////////////////////////////////////
+
+
+
+
 $cuviculo = $_GET['cuviculo'];
 $detalle = "Servicio de Parqueo de ".$tiempo;
-$precio = $_GET['precio'];
 
-/*$cantidad = $_GET['cantidad'];
-$total = $_GET['total'];
-$monto_total = $_GET['monto_total'];
-$monto_literal = $_GET['monto_literal'];
+/////////////////////////calcula el precio del cliente en horas/////////////////////
+$query_precios_horas = $pdo->prepare("SELECT * FROM tb_precios WHERE cantidad = '$hora_calculado' AND  detalle IN ('HORA', 'HORAS') AND estado = '1' ");
+        $query_precios_horas ->execute();
+        $datos_precios_horas = $query_precios_horas->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($datos_precios_horas as $datos_precios_hora) {
+        $precios_horas = $datos_precios_hora['precio'];
+        }
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////calcula el precio del cliente en días/////////////////////
+$precios_días =0;
+$query_precios_días = $pdo->prepare("SELECT * FROM tb_precios WHERE cantidad = '$día_imprimir_dif' AND detalle IN ('DIA', 'DIAS') AND estado = '1' ");
+        $query_precios_días ->execute();
+        $datos_precios_días = $query_precios_días->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($datos_precios_días as $datos_precios_día) {
+           $precios_días = $datos_precios_día['precio'];
+        }
+////////////////////////////////////////////////////////////////////////////////////
+$precio_final = $precios_horas + $precios_días;
+
+
+$cantidad = "1";
+
+$total =($precio_final * $cantidad);
+
+
+
+$monto_total = $total;
+
+$monto_literal = numtoletras($monto_total);
+
 $user_sesion = $_GET['user_sesion'];
-$qr = $_GET['qr'];
+
+///////Rescata la informacion del cliente////////
+$query_clientes = $pdo->prepare("SELECT * FROM tb_clientes WHERE id_cliente = '$id_cliente' AND estado = '1' ");
+        $query_clientes ->execute();
+        $datos_clientes = $query_clientes->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($datos_clientes as $datos_cliente) {
+            $id_cliente = $datos_cliente['id_cliente'];
+            $nombre_cliente = $datos_cliente['nombre_cliente'];
+            $Dni_Ruc_cliente = $datos_cliente['Dni_Ruc_cliente'];
+            $Placa_auto = $datos_cliente['Placa_auto'];
+        }
+
+/////////////////////////////////////////
+$qr = "Factura realizada por el sistema de parqueo <b>TONY_TECH S.A.C.</b>  al <b>CLIENTE:</b> ".$nombre_cliente."  con <b>DNI/ RUC:</b> ".$Dni_Ruc_cliente."
+con el vehiculo de <b>PLACA:</b> ".$Placa_auto." y esta factura se genero con la <b>FECHA DE EMISION:</b> ".$Fecha_emision." a <b>HORAS:</b> ".$Hora_salida."";
 
 
 $sentencia = $pdo->prepare('INSERT INTO tb_facturaciones
 (id_informacion, nro_factura, id_cliente, Fecha_emision, Fecha_ingreso, Hora_ingreso, Fecha_salida, Hora_salida, tiempo, cuviculo, detalle, precio, cantidad, total, monto_total, monto_literal, user_sesion, qr, fyh_creacion, estado)
-VALUES ( :id_informacion,: nro_factura,: id_cliente,: Fecha_emision,: Fecha_ingreso,: Hora_ingreso,: Fecha_salida,: Hora_salida,: tiempo,: cuviculo,: detalle,: precio,: cantidad,: total,: monto_total,: monto_literal,: user_sesion,: qr,:fyh_creacion,:estado)');
+VALUES ( :id_informacion, :nro_factura, :id_cliente, :Fecha_emision, :Fecha_ingreso, :Hora_ingreso, :Fecha_salida, :Hora_salida, :tiempo, :cuviculo, :detalle, :precio, :cantidad, :total, :monto_total, :monto_literal, :user_sesion, :qr, :fyh_creacion, :estado)');
 
 $sentencia->bindParam('id_informacion',$id_informacion);
 $sentencia->bindParam('nro_factura',$nro_factura);
@@ -51,7 +119,7 @@ $sentencia->bindParam('Hora_salida',$Hora_salida);
 $sentencia->bindParam('tiempo',$tiempo);
 $sentencia->bindParam('cuviculo',$cuviculo);
 $sentencia->bindParam('detalle',$detalle);
-$sentencia->bindParam('precio',$precio);
+$sentencia->bindParam('precio',$precio_final);
 $sentencia->bindParam('cantidad',$cantidad);
 $sentencia->bindParam('total',$total);
 $sentencia->bindParam('monto_total',$monto_total);
@@ -60,11 +128,14 @@ $sentencia->bindParam('user_sesion',$user_sesion);
 $sentencia->bindParam('qr',$qr);
 $sentencia->bindParam('fyh_creacion',$fechaHora);
 $sentencia->bindParam('estado',$estado_del_registro);
-*/
-//if($sentencia->execute()){
-//echo 'success';
-//header('Location:' .$URL.'/');
-//}else{
-//echo 'error al registrar a la base de datos';
-//}
+
+if($sentencia->execute()){
+echo 'success';
+
+?>
+    <script>location.href = "facturacion/factura.php";</script>
+<?php
+}else{
+echo 'error al registrar a la base de datos';
+}
 
--- facturacion/factura.php
+++ facturacion/factura.php
@@ -19,10 +19,49 @@ $query_informacions = $pdo->prepare("SELECT * FROM tb_informaciones WHERE estado
           $pais=$informacion['pais'];
 		}
 
+//Recuperar la informacion de la facura
+$query_facturas = $pdo->prepare("SELECT * FROM tb_facturaciones WHERE estado = '1' "); // consula y seleccion de la tb_informacioons
+        $query_facturas ->execute(); //ejecutar
+        $facturas = $query_facturas->fetchAll(PDO::FETCH_ASSOC);// vaciar con el forech
+        foreach ($facturas as $factura) {
+          $id_facturacion=$factura['id_facturacion'];
+          $id_informacion=$factura['id_informacion'];
+          $nro_factura=$factura['nro_factura'];
+          $id_cliente=$factura['id_cliente'];
+          $Fecha_emision=$factura['Fecha_emision'];
+          $Fecha_ingreso=$factura['Fecha_ingreso'];
+          $Hora_ingreso=$factura['Hora_ingreso'];
+          $Fecha_salida=$factura['Fecha_salida'];
+          $Hora_salida=$factura['Hora_salida'];
+          $tiempo=$factura['tiempo'];
+          $cuviculo=$factura['cuviculo'];
+          $detalle=$factura['detalle'];
+          $precio=$factura['precio'];
+          $cantidad=$factura['cantidad'];
+          $total=$factura['total'];
+          $monto_total=$factura['monto_total'];
+          $monto_literal=$factura['monto_literal'];
+          $user_sesion=$factura['user_sesion'];
+          $qr=$factura['qr'];
+        }
+/////////////////////////////////////////
+///////Rescata la informacion del cliente////////
+$query_clientes = $pdo->prepare("SELECT * FROM tb_clientes WHERE id_cliente = '$id_cliente' AND estado = '1' ");
+        $query_clientes ->execute();
+        $datos_clientes = $query_clientes->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($datos_clientes as $datos_cliente) {
+            $id_cliente = $datos_cliente['id_cliente'];
+            $nombre_cliente = $datos_cliente['nombre_cliente'];
+            $Dni_Ruc_cliente = $datos_cliente['Dni_Ruc_cliente'];
+            $Placa_auto = $datos_cliente['Placa_auto'];
+        }
+/////////////////////////////////////////
+
+
 
 
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, array(79,179), true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, array(79,182), true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
@@ -76,17 +115,17 @@ $html = '
 	<b>Teléfono:</b> '.$telefono.'<br>
 	'.$departamento_ciudad.'-'.$pais.'<br>
     -----------------------------------------------------------------------------------
-    <b>FACTURA N°</b> E001-1<br>
+    <b>FACTURA N°</b> E000-'.$nro_factura.'<br>
 	-----------------------------------------------------------------------------------
 	<div style="text-align: left">
 	<b>DATOS DEL CLIENTE</b><br>
-	<b>SEÑOR (A):</b> HIPOLO MARCELO ANTONY<br>
-	<b>DNI/RUC:</b> 74690220<br>
-	<b>Fecha de Emisión:</b> 74690220
+	<b>SEÑOR (A):</b> '.$nombre_cliente.'<br>
+	<b>DNI/RUC:</b> '.$Dni_Ruc_cliente.'<br>
+	<b>Fecha de Emisión:</b> '.$Fecha_emision.'
 	-----------------------------------------------------------------------------------<br>
-	<b>De: </b>24/05/2025 <b>Hora:</b> 20:48<br>
-	<b>A:  </b> 24/05/2025 <b>Hora:</b> 22:48<br>
-	<b>Tiempo:  </b> 2 horas en el Cuviculo 10
+	<b>De: </b>'.$Fecha_ingreso.' <b>Hora:</b> '.$Hora_ingreso.'<br>
+	<b>A:  </b> '.$Fecha_salida.' <b>               Hora:</b> '.$Hora_salida.'<br>
+	<b>Tiempo:  </b> '.$tiempo.'  en el Cuviculo '.$cuviculo.' 
     -----------------------------------------------------------------------------------<br>
     <table border="1" cellpadding="3">
     <tr>
@@ -96,22 +135,20 @@ $html = '
         <td style="text-align: center" width="45px"><b>Total</b></td>
     </tr>
     <tr>
-        <td>Servicio de Parqueo de 2 horas</td>
-        <td style="text-align: center">S/ 10.00</td>
-        <td style="text-align: center">1</td>
-        <td style="text-align: center">S/ 10.00</td>
+        <td>'.$detalle.'</td>
+        <td style="text-align: center">S/ '.$precio.'.00</td>
+        <td style="text-align: center">'.$cantidad.'</td>
+        <td style="text-align: center">S/ '.$total.'.00</td>
     </tr>
     </table>
     <p style="text-align: right">
-    <b>Monto Total: </b> S/ 10.00
+    <b>Monto Total: </b> S/ '.$monto_total.'.00
     </p>
     
-    <b> SON:</b>Diez 00/100 Soles.<br>
-    
+    <b>SON:</b>'.$monto_literal.'
     -----------------------------------------------------------------------------------
-	<b>USUARIO (A):</b> HIPOLO MARCELO<br><br><br><br><br><br><br><br>
+	<b>USUARIO(A):</b> '.$user_sesion.'<br><br><br><br><br><br><br><br><br>
     <p style="text-align: center">
-    
     </p>
     <p style="text-align: center">"ESTA FACTURA CONTRIBUYE AL DESARROLLO DEL PAÍS, EL USO ILÍCITO SERÁ SANCIONADO PENALMENTE DE ACUERDO A LEY"
     </p>
@@ -135,9 +172,8 @@ $style = array(
     'module_height' => 1 // height of a single module in points
 );
 
-$QR = 'Factura realizada por el sistema de paqueo PERU CARS. S.A.C., al cliente Antony hipolo con Dni/ Ruc: 74690220 
-con el vehiculo con numero de placa 3983FREDD y esta factura se genero en 26 de mayo de 2025 a hr: 19:11';
-$pdf->write2DBarcode($QR,'QRCODE,L',  22,110,35,35, $style);
+
+$pdf->write2DBarcode($qr,'QRCODE,L',  22,111,35,35, $style);
 
 
 
@@ -148,7 +184,7 @@ $pdf->write2DBarcode($QR,'QRCODE,L',  22,110,35,35, $style);
 
 
 //Close and output PDF document
-$pdf->Output('example_002.pdf', 'I');
+$pdf->Output('Factura.pdf', 'I');
 
 //============================================================+
 // END OF FILE
